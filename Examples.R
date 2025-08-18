c54307c66744b55525336e46ed64195f9d9c401d
Brystol Bates <brystol.bates@emory.edu>
2025-08-18 14:42:55 -0400

    table 1 created basic

---
+++ Examples.R
@@ -0,0 +1,39 @@
+library(tidyverse)
+library(gtsummary)
+
+# Load and clean data
+nlsy_cols <- c(
+	"glasses", "eyesight", "sleep_wkdy", "sleep_wknd",
+	"id", "nsibs", "samp", "race_eth", "sex", "region",
+	"income", "res_1980", "res_2002", "age_bir"
+)
+nlsy <- read_csv(here::here("data", "raw", "nlsy.csv"),
+								 na = c("-1", "-2", "-3", "-4", "-5", "-998"),
+								 skip = 1, col_names = nlsy_cols
+) |>
+	mutate(
+		region_cat = factor(region, labels = c("Northeast", "North Central", "South", "West")),
+		sex_cat = factor(sex, labels = c("Male", "Female")),
+		race_eth_cat = factor(race_eth, labels = c("Hispanic", "Black", "Non-Black, Non-Hispanic")),
+		eyesight_cat = factor(eyesight, labels = c("Excellent", "Very good", "Good", "Fair", "Poor")),
+		glasses_cat = factor(glasses, labels = c("No", "Yes"))
+	)
+
+# add labels for the variables and for the "missing" category
+tbl_summary(
+	nlsy,
+	by = sex_cat,
+	include = c(
+		sex_cat, race_eth_cat, region_cat,
+		sleep_wkdy, sleep_wknd, income
+	),
+	label = list(
+		race_eth_cat ~ "Race/ethnicity",
+		region_cat ~ "Region",
+		sleep_wkdy ~ "Sleep Weekday",
+		sleep_wknd ~ "Sleep Weekend",
+		income ~ "Income"
+	),
+	missing_text = "Missing"
+)
+
